f99b1f8fb210e7d779b57bec29b6895760b2bc07
scastillonu@uniminuto.edu.co <scastillonu@uniminuto.edu.co>
2021-01-26 23:08:15 -0500

    watchers option api

--- options/product.js
+++ options/product.js
@@ -1,6 +1,5 @@
 app.component("product", {
-
-    template: /* vue-html */ `
+  template: /* vue-html */ `
         <section class="product">
             <div class="product__thumbnails">
                 <div 
@@ -33,37 +32,42 @@ app.component("product", {
         </section>
     `,
 
-    props: ["product"],
+  props: ["product"],
 
-    emits: ["sendtocart"],
+  emits: ["sendtocart"],
 
-    data() {
-        return {
-            activeImage: 0,
-            discountCodes: ["PLATZI20", "RETAXMASTER"]
-        }
+  data() {
+    return {
+      activeImage: 0,
+      discountCodes: ["PLATZI20", "RETAXMASTER"],
+
+      price_color: "#369E2A",
+    };
+  },
+
+  methods: {
+    applyDiscount(event) {
+      const discountCodeIndex = this.discountCodes.indexOf(event.target.value);
+
+      if (discountCodeIndex >= 0) {
+        this.product.price *= 50 / 100;
+        this.discountCodes.splice(discountCodeIndex, 1);
+      }
     },
 
-    methods: {
-
-        applyDiscount(event) {
-
-            const discountCodeIndex = this.discountCodes.indexOf(event.target.value);
-
-            if (discountCodeIndex >= 0) {
-                this.product.price *= 50/100;
-                this.discountCodes.splice(discountCodeIndex, 1)
-            }
-
-        },
-
-        sendToCart() {
-    
-            this.$emit("sendtocart", this.product);
-    
-        }
+    sendToCart() {
+      this.$emit("sendtocart", this.product);
+    },
+  },
+  watch:{
+      activeImage(value, oldValue){
+          console.log(value);
         
-    },
-
-
-});
+      },
+      "product.stock"(stock){
+          if(stock<=1){
+            this.price_color='#9E312A'
+          }
+      }
+  }
+})
